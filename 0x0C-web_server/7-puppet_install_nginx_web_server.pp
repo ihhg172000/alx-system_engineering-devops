3d57ce6ee75ba3346b23ff88bfdb9a3beebeadc7
Islam Hany <ihhg.developer@gmail.com>
2023-09-27 13:08:39 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,15 +1,18 @@
-# conf nginx
+# nginx web server config
+exec { 'update' :
+  command  => 'apt-get -y update',
+  provider => 'shell',
+}
+
 package { 'nginx' :
   ensure => installed,
 }
 
 file { '/var/www/html/hello_world.html' :
-  ensure  => present,
   content => 'Hello World!',
 }
 
 file { '/var/www/html/404.html' :
-  ensure  => present,
   content => 'Ceci n\'est pas une page',
 }
 
@@ -21,18 +24,23 @@ server {
         root /var/www/html;
         index hello_world.html;
 
-	rewrite ^\/redirect_me http:\/\/youtube.com\/ permanent;
+	rewrite ^/redirect_me http://youtube.com/ permanent;
 
-	error_page 404 \/404.html;
+	error_page 404 /404.html;
 
         server_name _;
 
         location / {
-                try_files $uri $uri/ =404;
+                try_files \$uri \$uri/ =404;
         }
 }
 "
 
 file { '/etc/nginx/sites-available/default' :
   content => $conf,
+}
+
+exec { 'nginx restart' :
+  command  => 'service nginx restart',
+  provider => 'shell',
 }
