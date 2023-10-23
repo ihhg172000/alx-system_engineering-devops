6c53fe255d6b685ae16c8a9780684cea7b781982
Islam Hany <ihhg.developer@gmail.com>
2023-10-23 14:05:08 +0300

    Fix pip8 python style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
  using this REST API, for a given employee ID,
  returns information about his/her TODO list progress.
 """
-from sys import argv
 from requests import get
+from sys import argv
 
 
 if __name__ == '__main__':
